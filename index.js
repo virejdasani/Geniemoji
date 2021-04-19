32034fbb7a69279f05a9e843f2f8cc300cef66c6
Virej Dasani <dasanivirej@gmail.com>
2021-04-19 17:59:42 +0530

    Added emojis and emoji search functionality

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
+var searchCommand
+
 // This is executed when the ask button is pressed
-document.getElementById("showAnswer").addEventListener("click", function(event){
+document.getElementById("showAnswer").addEventListener("click", function (event) {
     // This prevents refresh
     event.preventDefault()
 
-    
-}) // askBtn => on click function
+    // Get the value of the search input
+    searchCommand = document.getElementById("commandInput").value.toLowerCase()
+
+    let answerEmojis
+
+    // For each emoji in the emojis.js file, this will search
+    emojis.forEach((item) => {
+        // This is executed if emojis.keywords (from emojis.js) has the same word as the user input
+        if ((item.keywords).includes(searchCommand)) {
+            // All the matching emojis are appended into answerEmojis
+            answerEmojis += item.char;
+
+            // answerEmojis returns 'undefined' before all the emojis. When this happens, the code below removes it
+            if (answerEmojis.includes('undefined')) {
+                answerEmojis = answerEmojis.replace('undefined', '')
+            }
+        }
+    });
+    // Alerts all the matching emojis
+    alert(answerEmojis);
+
+
+}) 
+// TODO - make it hot reload instead of showing results when enter is pressed
