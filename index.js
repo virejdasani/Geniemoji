37118b30951d2e77b9bda6b101e48e31d3af0e7b
Virej Dasani <dasanivirej@gmail.com>
2021-04-19 19:37:21 +0530

    The app now tells the user is their search doesn't match an emoji

--- index.js
+++ index.js
@@ -14,18 +14,26 @@ document.getElementById("showAnswer").addEventListener("click", function (event)
     emojis.forEach((item) => {
         // This is executed if emojis.keywords (from emojis.js) has the same word as the user input
         if ((item.keywords).includes(searchCommand)) {
+
             // All the matching emojis are appended into answerEmojis
             answerEmojis += item.char;
-
-            // answerEmojis returns 'undefined' before all the emojis. When this happens, the code below removes it
-            if (answerEmojis.includes('undefined')) {
-                answerEmojis = answerEmojis.replace('undefined', '')
-            }
         }
-    });
-    // Alerts all the matching emojis
-    alert(answerEmojis);
+    })
 
+    // If there are no matching emojis
+    if (typeof(answerEmojis) !== 'string') {
+        answerEmojis = 'No matching emojis found 😢'
+        console.log('if')
+        typeof(answerEmojis)
+    }
+
+    // answerEmojis returns 'undefined' before all the emojis. This is probably a zero index error but this works for now. Whenever this happens, the code below removes 'undefined' from the answer string
+    if (answerEmojis.includes('undefined')) {
+        answerEmojis = answerEmojis.replace('undefined', '')
+    }
+
+    // Displays all the matching emojis in the answer html div
+    document.getElementById('answer').innerHTML = answerEmojis
 
 }) 
 // TODO - make it hot reload instead of showing results when enter is pressed
