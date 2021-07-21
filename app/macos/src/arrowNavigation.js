e51b04f25dd3597a682409b38e00eeb533668454
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-07-21 15:00:51 +0530

    Made code more efficient

--- app/macos/src/arrowNavigation.js
+++ app/macos/src/arrowNavigation.js
@@ -1,36 +1,24 @@
 // For arrow key navigation
 document.addEventListener("keydown", (event) => {
   // Key is ArrowUp or ArrowDown?
-  if (event.code === "ArrowDown" || event.code === "ArrowUp") {
+  if (
+    event.code === "ArrowDown" ||
+    event.code === "ArrowUp" ||
+    event.code === "ArrowLeft" ||
+    event.code === "ArrowRight"
+  ) {
     event.preventDefault();
     // get tabIndex of current element
     let tabIndex = event.target.tabIndex;
-    // increment or decrement tabindex depending on Key (ArrowUp -> previous Element, ArrowDown -> next element)
-    tabIndex += event.code === "ArrowUp" ? -1 : 1;
 
-    // circle through emojis
-    // ArrowUp and focus on input field? -> select last emoji
-    if (tabIndex < 1) {
-      tabIndex = currentEmojiLength + 2; // '+2': tabIndex starts with 1, 1 = input
+    // Increment or decrement tabIndex depending on Key (ArrowUp/Left -> previous Element, ArrowDown/Right -> next element)
+    if (event.code === "ArrowUp" || event.code === "ArrowLeft") {
+      tabIndex += -1;
+    } else if (event.code === "ArrowDown" || event.code === "ArrowRight") {
+      tabIndex += 1;
     }
-    // ArrowDown and focus on last emoji? -> select input field
-    if (tabIndex > currentEmojiLength + 2) {
-      tabIndex = 1;
-    }
-    // get element with newly calculated tabindex
-    const newEl = document.querySelector(`[tabindex="${tabIndex}"]`);
-    // set focus on element to select
-    newEl.focus();
-  }
-  // Key is ArrowLeft or ArrowRight?
-  else if (event.code === "ArrowLeft" || event.code === "ArrowRight") {
-    event.preventDefault();
-    // get tabIndex of current element
-    let tabIndex = event.target.tabIndex;
-    // increment or decrement tabindex depending on Key (ArrowUp -> previous Element, ArrowDown -> next element)
-    tabIndex += event.code === "ArrowLeft" ? -1 : 1;
 
-    // circle through emojis
+    // Circle through emojis
     // ArrowUp and focus on input field? -> select last emoji
     if (tabIndex < 1) {
       tabIndex = currentEmojiLength + 2; // '+2': tabIndex starts with 1, 1 = input
